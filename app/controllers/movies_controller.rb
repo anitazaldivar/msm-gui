9b8c55eec32684055e0acee375d01c6b8a932c3f
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-05-01 13:39:44 +0000

    starting update button

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -49,4 +49,28 @@ class MoviesController < ApplicationController
 
     redirect_to("/movies")
   end
+
+  def update
+    # Params hash: {"the_title"=>"", "the_year"=>"", "the_duration"=>"", "the_description"=>"", "the_image"=>"", "the_director_id"=>"", "an_id"=>"1"}
+    # Get the ID out of params
+    m_id = params.fetch("an_id")
+
+    # Look the existing record
+    matching_records = Movie.where({ :id => m_id})
+    the_movie = matching_records.at(0)
+
+    # Overwrite each column with values from user inputs
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    # Save
+    the_movie.save
+
+    # Redirect to movie details page
+    redirect_to("movies/m_id")
+  end
 end
